0f8428005a119f7f717795bcd13444ef942ccc90
Stefan Eng <stefaneng13@gmail.com>
2015-10-29 11:20:41 -0700

    Add history component

---
+++ app/History/History.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './History.css';
+
+export default class History extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div className={styles.app}>
+            History
+        </div>
+    );
+  }
+}
