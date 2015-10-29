322513abe2280bba9206845821e6bfb9560ab4a0
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2015-10-29 11:57:36 -0700

    Create Queue component.

---
+++ app/Queue/Queue.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Queue extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+            Queue
+        </div>
+    );
+  }
+}
