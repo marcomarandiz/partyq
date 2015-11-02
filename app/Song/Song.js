65b297ae0225ab2fed77d083db7e1574e90b4e00
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2015-11-02 13:14:07 -0800

    Apparently I changed a file so another commit.

--- app/Song/Song.js
+++ app/Song/Song.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './Song.css';
 
 export default class Song extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className={styles.song}>
-				Im a song listen to me
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={styles.song}>
+        Im a song listen to me
+      </div>
+    );
+  }
 }
