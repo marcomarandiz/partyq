8794e0b48b2acc637a2bf81c91abdaabfab91726
vitalxx <amaynard.x@gmail.com>
2015-11-03 11:08:15 -0800

    Added current song component

---
+++ app/CurrentSong/CurrentSong.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './CurrentSong.css';
+
+export default class CurrentSong extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={styles.currentSong}>
+        current song
+      </div>
+    );
+  }
+}
