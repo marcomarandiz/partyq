2e69c6652057879c89f22cdc6bda3be663883498
Marco Jonker <marco.a.jonker@gmail.com>
2015-11-06 22:08:29 -0800

    addmodal txt input and addsong button placed in layout

--- app/AddSong/AddSong.js
+++ app/AddSong/AddSong.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import styles from './AddSong.css';
-import addModal from './addModal.js';
 
 export default class AddSong extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick =  function(i) {
-        console.log('You clicked: addmodal');
-    };
   }
   render() {
     return (
