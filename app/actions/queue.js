e2528067485844b369382c3f21dcb7df95d8f701
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2015-11-07 12:59:13 -0800

    Changed song object returned in queue.js so it has fields song_name and upvotes. Changed reducer test for new song object. It passes tests right now and website display is correct but I'm not sure if I implemented this correctly. Still need to make and test upvote action.

--- app/actions/queue.js
+++ app/actions/queue.js
@@ -1,8 +1,8 @@
 import * as types from '../constants/ActionTypes';
 
-export function addSong(song) {
+export function addSong(song_name) {
   return {
     type: types.ADD_SONG,
-    song: song
+    song: {song_name: song_name, upvotes: 0}
   };
 }
--- app/actions/song.js
+++ app/actions/song.js
@@ -1,8 +1,8 @@
 import * as types from '../constants/ActionTypes';
 
-export function upvoteSong (song){
-	return {
-		type: types.UPVOTE_SONG;
-    song: song
-	};
+export function upvoteSong(song) {
+  return {
+    type: types.UPVOTE_SONG,
+    {song: 'song_name', upvotes: 0}
+  };
 }
