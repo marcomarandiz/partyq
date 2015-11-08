468e294796da333ece2a777eb7d52137cdccb401
Stefan Eng <stefaneng13@gmail.com>
2015-11-07 20:40:23 -0800

    Add upvote song test

--- app/test/reducer_spec.js
+++ app/test/reducer_spec.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import reducer from '../reducer';
 import { addSong } from '../actions/queue';
+import { upvoteSong } from '../actions/song';
 
 describe('reducer', () => {
   it('handles ADD_SONG', () => {
@@ -10,6 +11,27 @@ describe('reducer', () => {
 
     expect(nextState).to.deep.equal({
       queueSonglist: [{song_name: 'song-one', upvotes: 0}]
+    });
+  });
+
+  it('upvotes song with UPDATE_SONG', () => {
+    const initialState = {
+      queueSonglist: [
+        {
+          song_name: 'song', upvotes: 0
+        }
+      ]
+    };
+    const action = upvoteSong(0);
+    const nextState = reducer(initialState, action);
+
+
+    expect(nextState).to.deep.equal({
+      queueSonglist: [
+        {
+          song_name: 'song', upvotes: 1
+        }
+      ]
     });
   });
 });
