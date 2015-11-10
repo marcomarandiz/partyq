179959079b5e5410f0240d41bff3ec541bb43d46
vitalxx <amaynard.x@gmail.com>
2015-11-10 14:42:06 -0800

    upvote icon, some other stuff

--- app/components/Song/Song.js
+++ app/components/Song/Song.js
@@ -9,8 +9,13 @@ export default class Song extends React.Component {
   render() {
     return (
       <div className={styles.song}>
-
-        <a href='#' onClick={() => this.props.handleUpvote(this.props.index)}>asdf</a>&nbsp;&nbsp;&nbsp;index: {this.props.index}
+        <a href='#' onClick={() => this.props.handleUpvote(this.props.index)}>
+          <img src='https://emoji.slack-edge.com//T0BPYMCNQ//steve//e3c6ac7cd44a97cf.jpg' alt='upvote' width='5%' height='5%'/>
+        </a>
+      {/*this is nasty and hardcoded, need to figure out how to fix spacing*/}
+        &nbsp;
+        &nbsp;
+        &nbsp;
         {this.props.song.song_name}: {this.props.song.upvotes}
       </div>
     );
