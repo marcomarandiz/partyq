2f9b5a834c51bf6549a3d6c29bb33e256a17e183
Marco Jonker <marco.a.jonker@gmail.com>
2015-11-13 19:48:33 -0800

    more layout stuff smh

--- app/components/Song/Song.js
+++ app/components/Song/Song.js
@@ -11,9 +11,11 @@ export default class Song extends React.Component {
       <div className={styles.song}>
             <div className={styles.upvoteDiv}>
                 <a href='#' onClick={() => this.props.handleUpvote(this.props.index)} className={styles.upvoteButton}>
-                  <img src={require('./upvote-icon.png')} alt='upvote' width='10%' height='10%'/>
-                </a><br/>
-                {this.props.song.upvotes}
+                  <img src={require('./upvote-icon.png')} alt='upvote' width='1%' height='1%'/>
+                </a>
+                <div>
+                    {this.props.song.upvotes}
+                </div>
             </div>
       {/* this is nasty and hardcoded, need to figure out how to fix spacing */}
         &nbsp;
@@ -23,8 +25,8 @@ export default class Song extends React.Component {
             <img className={styles.thumbnail} src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Q_logo_2015.svg/2000px-Q_logo_2015.svg.png' alt='thumbnail'/>
         </div>
         <div className={styles.songInfo}>
-            <span className={styles.title}>Title</span>
-            <span className={styles.artst}>Artist</span>
+            <div className={styles.title}>Title</div>
+            <div className={styles.artst}>Artist</div>
         </div>
         <span className={styles.durationDiv}>5:43</span>
       </div>
