7767b3a84ad4129b4efae27317783671c31cb68c
vitalxx <amaynard.x@gmail.com>
2015-11-17 17:52:30 -0800

    removed unneeded modal stuff

--- app/components/AddSong/AddSong.js
+++ app/components/AddSong/AddSong.js
@@ -16,16 +16,6 @@ export default class AddSong extends React.Component {
     this.modal();
   }
 
-  modal() {
-    $('.ui.basic.modal')
-      .modal({
-        selector: { 
-          close: 'icon.checkmark'
-        } 
-     }).modal('show')
-    ;
-  }
-
   render() {
     return (
       <div className={styles.addsong}>
@@ -35,15 +25,7 @@ export default class AddSong extends React.Component {
               <button className='ui button' onClick={() => this.handeClick(event)}>Add Song</button>
             </div>
           </form>
-
-          <div className={'ui basic modal ' + styles.modal}>
-            <div className={'actions ' + styles.modal}>
-              <div className={'ui green basic inverted positive button ' + styles.modal}>
-                <i className={'checkmark icon ' + styles.modal}></i>Song Added
-              </div>
-            </div>
-          </div>
-        </div>
+      </div>
     );
   }
 }
--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
     .sticky({
       context: 'body'
     });
-  };
+  }
 
 
   render() {
