d03bab6402950bd38b355e87d54eeb77c320afff
Stefan Eng <stefaneng13@gmail.com>
2015-11-21 20:05:50 -0800

    Remove old out of date comments

--- server/server.js
+++ server/server.js
@@ -13,12 +13,6 @@ export default function startServer(store) {
     () => partyq.emit('state', store.getState())
   );
 
-  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=nfWlot6h_JM'});
-  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=4d2lGAP5xvQ'});
-  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=PhRa3REdozw'});
-  // Upvoting in here currently breaks ADD_SONG pulling info from youtube
-  // store.dispatch({type: 'UPVOTE_SONG', index: 0});
-
   partyq.on('connection', (socket) => {
     socket.emit('state', store.getState());
     // Feed action event from clients directly into store
