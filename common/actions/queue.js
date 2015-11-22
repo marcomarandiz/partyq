059e9c20976bfaf3c81ad31d0b3f4d3016e7d7d7
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2015-11-21 17:57:12 -0800

    Use socketio as middleware to handle API calls. When it catches an ADD_SONG action it calls the youtubeAPI.

--- common/actions/queue.js
+++ common/actions/queue.js
@@ -1,8 +1,10 @@
 import * as types from '../constants/ActionTypes';
+import { getVidFromUrl } from '../utils/functions.js';
 
 export function addSong(url) {
   return {
     type: types.ADD_SONG,
-    url
+    url,
+    vid: getVidFromUrl(url)
   };
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import Server from 'socket.io';
+import { youtubeAPI, handleAPIError } from './utils/APIcalls.js';
 
 const development = process.env.NODE_ENV !== 'production';
 
@@ -12,9 +13,9 @@ export default function startServer(store) {
     () => partyq.emit('state', store.getState())
   );
 
-  store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=nfWlot6h_JM'});
-  store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=4d2lGAP5xvQ'});
-  store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=PhRa3REdozw'});
+  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=nfWlot6h_JM'});
+  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=4d2lGAP5xvQ'});
+  // store.dispatch({type: 'ADD_SONG', url: 'https://www.youtube.com/watch?v=PhRa3REdozw'});
   // Upvoting in here currently breaks ADD_SONG pulling info from youtube
   // store.dispatch({type: 'UPVOTE_SONG', index: 0});
 
@@ -23,13 +24,28 @@ export default function startServer(store) {
     // Feed action event from clients directly into store
     // Should probably put authentication here
     socket.on('action', (action) => {
+
       // Attach the remote address as id so we know who performed the actions
       // If we are in production, use the ip address
       // If we are developing, use the socket id
       action.id = development ? socket.id :
         socket.request.connection.remoteAddress;
 
-      store.dispatch.bind(store)(action);
+      // Checks if action is 'ADD_SONG' and if it is
+      // it calls the youtubeAPI and if it is and makes
+      // a callback to handle errors or dispatch the song
+      if (action.type === 'ADD_SONG') {
+        youtubeAPI(action.url, action.vid, (error, song) => {
+          if (error) {
+            handleAPIError(error);
+          } else {
+            action.song = song;
+            store.dispatch.bind(store)(action);
+          }
+        });
+      } else {
+        store.dispatch.bind(store)(action);
+      }
     });
   });
 }
