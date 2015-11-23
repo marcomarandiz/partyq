6a1ea80dd28fa4f7acc7879f1cad7fdf36e11854
Stefan Eng <stefaneng13@gmail.com>
2015-11-22 21:02:06 -0800

    classNames in Song

--- app/components/Song/Song.js
+++ app/components/Song/Song.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import styles from './Song.css';
+import classNames from 'classnames';
 
 export default class Song extends React.Component {
   constructor(props) {
@@ -8,30 +9,34 @@ export default class Song extends React.Component {
 
   render() {
     return (
-      <div className={'ui grid segment red ' + styles.song}>
-          <div className='one wide column center aligned'>
+      <div className={classNames('ui', 'grid', 'segment', 'red ', styles.song)}>
+          <div className={classNames('one', 'wide', 'column', 'center', 'aligned')}>
             <div>
-              <i className={'big white angle up link icon ' + styles.upvoteButton} onClick={() => this.props.handleUpvote(this.props.index)}></i>
+              <i className={classNames('big', 'white', 'angle', 'up', 'link', 'icon', styles.upvoteButton)}
+                 onClick={() => this.props.handleUpvote(this.props.index)}></i>
             </div>
             <div>
               {this.props.song.upvotes}
             </div>
           </div>
-          <div className='five wide column'>
-                  <div className={styles.thumbnail}>
-                      <a href={this.props.song.url}><img src={this.props.song.thumbnail} className={styles.ytThumb} /></a>
+          <div className={classNames('five', 'wide', 'column')}>
+                  <div className={classNames(styles.thumbnail)}>
+                      <a href={this.props.song.url}>
+                        <img src={this.props.song.thumbnail}
+                             className={classNames(styles.ytThumb)} />
+                      </a>
                   </div>
-                <div className={styles.songInfo}>
-                  <div className={styles.title}>
+                <div className={classNames(styles.songInfo)}>
+                  <div className={classNames(styles.title)}>
                     {this.props.song.title ? this.props.song.title : 'Unknown title'}
                   </div>
-                  <div className={styles.artist}>
+                  <div className={classNames(styles.artist)}>
                     {this.props.song.artist ? this.props.song.artist : 'Unknown artist'}
                   </div>
                 </div>
           </div>
-          <div className='one wide column'>
-            <span className={styles.duration}>{this.props.song.duration}</span>
+          <div className={classNames('one', 'wide', 'column')}>
+            <span className={classNames(styles.duration)}>{this.props.song.duration}</span>
           </div>
       </div>
     );
