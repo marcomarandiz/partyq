455974b3b4c65ce1d37cfe3166c0d976c0577b05
Stefan Eng <stefaneng13@gmail.com>
2015-11-27 20:06:13 -0800

    youtu.be style link test

--- test/utils.js
+++ test/utils.js
@@ -8,14 +8,20 @@ describe('utils', () => {
     describe('youtube links', () => {
       it('handles multiple query strings', () => {
         const url = 'http://www.youtube.com/watch?v=abc123&feature=index';
-        expect(isLinkValid(url)).to.be.true;
+        expect(isLinkValid(url)).to.be.ok;
       });
 
       /* FAILING TEST */
       it('handles /v/id style links', () => {
         const url = 'https://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0';
 
-        expect(isLinkValid(url)).to.be.true;
+        expect(isLinkValid(url)).to.be.ok;
+      });
+
+      it('handles youtu.be/id links', () => {
+        const url = 'http://youtu.be/0zM3nApSvMg';
+
+        expect(isLinkValid(url)).to.be.ok;
       });
     });
   });
