1b9392f2b1e66dd716d36686162e0a0a667cfe01
Stefan Eng <stefaneng13@gmail.com>
2015-11-27 20:00:39 -0800

    Failing test of /v/<id>/... style links

--- test/utils.js
+++ test/utils.js
@@ -6,8 +6,15 @@ import { isLinkValid } from '../common/utils/functions';
 describe('utils', () => {
   describe('valid urls', () => {
     describe('youtube links', () => {
-      it('multiple query strings', () => {
+      it('handles multiple query strings', () => {
         const url = 'http://www.youtube.com/watch?v=abc123&feature=index';
+        expect(isLinkValid(url)).to.be.true;
+      });
+
+      /* FAILING TEST */
+      it('handles /v/id style links', () => {
+        const url = 'https://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0';
+
         expect(isLinkValid(url)).to.be.true;
       });
     });
