fd5f3019160fa3db4066757d0926e0a18f9bf6ce
Stefan Eng <stefaneng13@gmail.com>
2015-11-28 15:25:46 -0800

    Refactor urls outside of the it functions

--- test/utils.js
+++ test/utils.js
@@ -4,41 +4,37 @@ import { expect } from 'chai';
 import { isLinkValid } from '../common/utils/functions';
 
 describe('utils', () => {
+  const multipleQueryStringUrl = 'http://www.youtube.com/watch?v=abc123&feature=index';
+  const vIdUrl = 'https://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0';
+  const shortYoutubeUrl = 'http://youtu.be/0zM3nApSvMg';
+  const googleUrl = 'https://google.com';
+  const regularUrl = 'https://www.youtube.com/watch?v=mbyG85GZ0PI';
+  const embedUrl = 'http://www.youtube.com/embed/mbyG85GZ0PI?rel=0';
+
   describe('valid urls', () => {
     describe('youtube links', () => {
       it('handles multiple query strings', () => {
-        const url = 'http://www.youtube.com/watch?v=abc123&feature=index';
-        expect(isLinkValid(url)).to.be.ok;
+        expect(isLinkValid(multipleQueryStringUrl)).to.be.ok;
       });
 
       it('handles /v/id style links', () => {
-        const url = 'https://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0';
-
-        expect(isLinkValid(url)).to.be.ok;
+        expect(isLinkValid(vIdUrl)).to.be.ok;
       });
 
       it('handles youtu.be/id links', () => {
-        const url = 'http://youtu.be/0zM3nApSvMg';
-
-        expect(isLinkValid(url)).to.be.ok;
+        expect(isLinkValid(shortYoutubeUrl)).to.be.ok;
       });
 
       it('should not handle google links', () => {
-        const url = 'https://google.com';
-
-        expect(isLinkValid(url)).to.not.be.ok;
+        expect(isLinkValid(googleUrl)).to.not.be.ok;
       });
 
       it('handles regular single query string links', () => {
-        const url = 'https://www.youtube.com/watch?v=mbyG85GZ0PI';
-
-        expect(isLinkValid(url)).to.be.ok;
+        expect(isLinkValid(regularUrl)).to.be.ok;
       });
 
       it('handles embed links', () => {
-        const url = 'http://www.youtube.com/embed/mbyG85GZ0PI?rel=0';
-
-        expect(isLinkValid(url)).to.be.ok;
+        expect(isLinkValid(embedUrl)).to.be.ok;
       });
     });
   });
