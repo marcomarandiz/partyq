81bbb48755be01fbc218c0641c399dc9c9e8386a
vitalxx <amaynard.x@gmail.com>
2015-12-29 19:54:32 -0700

    cleaned up markup for historysong.js

--- app/components/HistorySong/HistorySong.js
+++ app/components/HistorySong/HistorySong.js
@@ -12,11 +12,9 @@ export default class HistorySong extends React.Component {
         <div className={classNames(styles.historyContent)}>
           {this.props.song.title} {this.props.song.artist} {this.props.song.endedAt}
         </div>
-        <button
-          className={classNames('ui', 'tiny', 'button', styles.button)}
-          type='button'
-          onClick={() => this.props.handleReAddSong(this.props.index)}>
-        Add to q
+        <button className={classNames('ui', 'tiny', 'button', styles.button)}
+          type='button' onClick={() => this.props.handleReAddSong(this.props.index)}>
+            Add to q
         </button>
       </div>
     );
