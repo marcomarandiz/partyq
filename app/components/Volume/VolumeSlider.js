b6c87fe4f5dd39d89a16639601a1a733d6a307a5
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-10 12:11:20 -0800

    Volume Slider rendered on hover over volume button.

--- app/components/Volume/VolumeSlider.js
+++ app/components/Volume/VolumeSlider.js
@@ -9,6 +9,7 @@ export default class VolumeSlider extends React.Component {
     // Deals this bug where the VolumeSlider doesn't get rendered
     // when it wasn't already rendered and a Soundcloud song is added
     // I believe it's related to Soundcloud songs not updating the view
+    this.toggleVolumeOff();
     this.forceUpdate();
   }
 
@@ -16,15 +17,30 @@ export default class VolumeSlider extends React.Component {
     this.props.changeVolume(volume);
   }
 
+  toggleVolumeOn() {
+    document.getElementById('volume').style.display = 'block';
+  }
+
+  toggleVolumeOff() {
+    document.getElementById('volume').style.display = 'none';
+  }
+
   render() {
     $('#volume').slider({
-      value: 50
+      value: 50,
+      orientation: 'vertical'
     });
     $('#volume').on('slide', (event, ui) => {
       this.setVolume(ui.value);
     });
     return (
       <div>
+        <input
+          type='button'
+          value='volume'
+          onMouseOver={() => this.toggleVolumeOn()}
+          onMouseOut={() => this.toggleVolumeOff()}
+        />
         <div id='volume' animate='true' default='50' />
       </div>
     );
