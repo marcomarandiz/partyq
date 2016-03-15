7c1459175ea4e5a10ced3a666ec67fe3b83ae223
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-14 20:57:01 -0700

    Started writing queries

--- server/utils/sqllib.js
+++ server/utils/sqllib.js
@@ -2,9 +2,25 @@ import pg from 'pg';
 
 const conString = process.env.PG_INFO;
 
+export function insertSongQuery(song) {
+  return ('INSERT INTO (sid, source, title, artist, duration, upvotes, skipvotes) VALUES ('
+      + song.id + ', ' + song.src + ', ' + song.title + ', ' + song.artist + ', '
+      + song.duration + ', 0, 0);');
+}
+
 pg.connect(conString, (err, client, done) => {
   if (err) {
     return console.error('error fetching client from pool', err);
   }
-  return console.log('no error');
+  const query = insertSongQuery({
+    id: 123456,
+    source: 'youtube',
+    title: 'Hello',
+    artist: 'Adele',
+    duration: 300
+  });
+  client.query(query, (error, result) => {
+    console.log(result);
+  });
 });
+
