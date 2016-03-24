6830ae2349bba5804a5202455320b961d79b78c5
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-24 16:46:45 -0700

    Created wrapper for database queries

--- server/utils/sqllib.js
+++ server/utils/sqllib.js
@@ -1,10 +1,27 @@
-// import pg from 'pg';
+import pg from 'pg';
 
-// const conString = process.env.PG_INFO;
+const conString = process.env.PG_INFO;
 // Sample constring
 // pg://Alex@localhost/mydb
 
 // console.log(conString);
+
+export function runQuery(ourQuery, next) {
+  pg.connect(conString, (err, client, done) => {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(ourQuery, (error, result) => {
+      if (error) {
+        return console.error('Error running query:', error);
+      } else {
+        next(result);
+        return; 
+      }
+    });
+    done();
+  });
+}
 
 export function getSidsFromRoomSongs(roomId) {
   return {
@@ -95,35 +112,3 @@ export function insertSongIntoSongs(song) {
   };
 }
 
-/*
-pg.connect(conString, (err, client, done) => {
-  if (err) {
-    return console.error('error fetching client from pool', err);
-  }
-  const song = {
-    id: 123456789,
-    src: 'youtube',
-    title: 'Hello',
-    artist: 'Adele',
-    duration: 300
-  };
-  client.query(insertSongIntoSongs(song), (error, result) => {
-    // console.log(result);
-    console.log(error);
-  });
-  const queryConfig = getSongBySid(song.id);
-  console.log(queryConfig);
-  client.query('SELECT * FROM songs WHERE sid = 123456789;', (error, result) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(result.rowCount);
-  });
-  client.query(queryConfig, (error, result) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(result.rowCount);
-  });
-});
-*/
