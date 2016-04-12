d89c0dcc307c95020f21be1e54af5e0af0b40d77
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-12 12:18:06 -0700

    New header plugged in to react

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './Header.css';
 import classNames from 'classnames';
-import AddSong from '../../components/AddSong/AddSong.js';
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -17,14 +16,34 @@ export default class Header extends React.Component {
 
   render() {
     return (
-      <div className={classNames('ui', 'sticky', styles.header)}>
-        <div className={classNames(styles.addSong)}>
-            <AddSong onAddSong={this.props.onAddSong} />
+      <nav id='mainNav' className={classNames('navbar', 'navbar-default', 'navbar-fixed-top')}>
+        <div className={classNames('container-fluid')}>
+          <div className={classNames('navbar-header')}>
+            <button type='button' className={classNames('navbar-toggle', 'collapsed')} data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+              <span className={classNames('sr-only')}>Toggle navigation</span>
+              <span className={classNames('icon-bar')}></span>
+              <span className={classNames('icon-bar')}></span>
+              <span className={classNames('icon-bar')}></span>
+            </button>
+            <a className={classNames('navbar-brand', 'page-scroll')} href='#page-top'>partyq.io</a>
+          </div>
+
+          <div className={classNames('collapse', 'navbar-collapse', styles.topnav)} id='top-nav'>
+            <ul className={classNames('nav navbar-nav', 'navbar-right')}>
+              <li>
+                <i className={classNames('inverted', 'blue', 'large', 'circular', 'info', 'icon')} style={{float: 'right'}}></i>
+              </li>
+              <li>
+                <i className={classNames('inverted', 'blue', 'large', 'circular', 'facebook', 'icon')} style={{float: 'right'}}></i>
+              </li>
+              <li>
+                <i className={classNames('inverted', 'blue', 'large', 'circular', 'twitter', 'icon')} style={{float: 'right'}}></i>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className={classNames('ui', 'small', 'image', styles.logo)}>
-          <img src={require('./partyqBLUE.png')}/>
-        </div>
-      </div>
+      </nav>
     );
   }
 }
+
