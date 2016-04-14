fd4c49f267b8fadf8f084498116a5222781a9614
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-13 18:44:21 -0700

    Got queue looking correctish. Problem was old css file for app

--- app/components/Song/Song.js
+++ app/components/Song/Song.js
@@ -9,35 +9,13 @@ export default class Song extends React.Component {
 
   render() {
     return (
-      <div className={classNames('ui', 'grid', 'red', 'segment', styles.song)}>
-          <div className={classNames('four', 'wide', 'column', 'center', 'aligned', styles.twosongbackground)}>
-            <div>
-              <i className={classNames('big', 'white', 'angle', 'up', 'link', 'icon', styles.upvoteButton)}
-                 onClick={() => this.props.handleUpvote(this.props.index)}></i>
-            </div>
-            <div>
-              {this.props.song.upvotes}
-            </div>
-          </div>
-          <div className={classNames('ten', 'wide', 'column', styles.fivesongbackground)}>
-                  <div className={classNames(styles.thumbnail)}>
-                      <a href={this.props.song.url}>
-                        <img src={this.props.song.thumbnail}
-                             className={classNames(styles.ytThumb)} />
-                      </a>
-                  </div>
-                <div className={classNames(styles.songInfo)}>
-                  <div className={classNames(styles.title)}>
-                    {this.props.song.title ? this.props.song.title : 'Unknown title'}
-                  </div>
-                  <div className={classNames(styles.artist)}>
-                    {this.props.song.artist ? this.props.song.artist : 'Unknown artist'}
-                  </div>
-                </div>
-          </div>
-          <div className={classNames('two', 'wide', 'column', styles.onesongbackground)}>
-            <span className={classNames(styles.duration)}>{this.props.song.duration}</span>
-          </div>
+      <div>
+        {this.props.song.upvotes}
+          <a href={this.props.song.url}>
+            <img src={this.props.song.thumbnail} className={classNames(styles.ytThumb)} />
+          </a>
+        {this.props.song.title ? this.props.song.title : 'Unknown Title'}
+        {this.props.song.artist ? this.props.song.artist : 'Unknown Artist'}
       </div>
     );
   }
@@ -51,3 +29,35 @@ Song.propTypes = {
     upvotes: PropTypes.number.isRequired
   })
 };
+
+
+      // <div className={classNames('ui', 'grid', 'red', 'segment', styles.song)}>
+      //     <div className={classNames('four', 'wide', 'column', 'center', 'aligned', styles.twosongbackground)}>
+      //       <div>
+      //         <i className={classNames('big', 'white', 'angle', 'up', 'link', 'icon', styles.upvoteButton)}
+      //            onClick={() => this.props.handleUpvote(this.props.index)}></i>
+      //       </div>
+      //       <div>
+      //         {this.props.song.upvotes}
+      //       </div>
+      //     </div>
+      //     <div className={classNames('ten', 'wide', 'column', styles.fivesongbackground)}>
+      //             <div className={classNames(styles.thumbnail)}>
+      //                 <a href={this.props.song.url}>
+      //                   <img src={this.props.song.thumbnail}
+      //                        className={classNames(styles.ytThumb)} />
+      //                 </a>
+      //             </div>
+      //           <div className={classNames(styles.songInfo)}>
+      //             <div className={classNames(styles.title)}>
+      //               {this.props.song.title ? this.props.song.title : 'Unknown title'}
+      //             </div>
+      //             <div className={classNames(styles.artist)}>
+      //               {this.props.song.artist ? this.props.song.artist : 'Unknown artist'}
+      //             </div>
+      //           </div>
+      //     </div>
+      //     <div className={classNames('two', 'wide', 'column', styles.onesongbackground)}>
+      //       <span className={classNames(styles.duration)}>{this.props.song.duration}</span>
+      //     </div>
+      // </div>
