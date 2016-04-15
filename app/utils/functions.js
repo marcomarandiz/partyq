26957cea7dd9192d429a0ddc5fc1d392552154c6
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-15 11:21:11 -0700

    Check if mobile function

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -1,2 +1,14 @@
 // frontend only functions go here
-module.exports = {};
+export function isMobile() {
+  if (navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+     ) {
+    return true;
+  }
+  return false;
+}
