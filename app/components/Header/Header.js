a5abb9da89fa04ab9f58c63cfd00a2f9040086fe
WTFranklin <faddelia@gmail.com>
2016-04-14 19:02:43 -0700

    header updates

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -25,7 +25,7 @@ export default class Header extends React.Component {
               <span className={classNames('icon-bar')}></span>
               <span className={classNames('icon-bar')}></span>
             </button>
-            <a className={classNames('navbar-brand', 'page-scroll')} href='#page-top'><img className={classNames(styles.logo)} src='http://i.imgur.com/ib1ZmYy.png'/></a>
+            <a className={classNames('navbar-brand', 'page-scroll')} href='#page-top'><img className={classNames(styles.logo)} width='125' height='54' src='http://i.imgur.com/ib1ZmYy.png'/></a>
           </div>
 
           <div className={classNames('collapse', 'navbar-collapse', styles.topnav)}>
